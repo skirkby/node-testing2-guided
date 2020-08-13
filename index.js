c318120769cf0a90273096a613cc87eb1a6a3f3b
Sean Kirkby <sean.kirkby@niceincontact.com>
2020-08-12 21:57:10 -0600

    production db

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const server = require('./api/server.js');
 // const server1 = require('./api/server.js');
 
-// console.log(`DB_ENV = ${process.env.DB_ENV}`);
+console.log(`DB_ENV = ${process.env.DB_ENV}`);
 
 
 const port = process.env.PORT || 5000;
