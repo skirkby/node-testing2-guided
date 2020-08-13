d0c68d37915caaf859a0da8a8ebf89db75c87ffe
Sean Kirkby <sean.kirkby@niceincontact.com>
2020-08-13 00:07:41 -0600

    The most technologically efficient machine that man has ever invented is the book.

--- hobbits/hobbitsModel.spec.js
+++ hobbits/hobbitsModel.spec.js
@@ -1,32 +1,40 @@
 const db = require('../data/dbConfig.js');
 const Hobbits = require('./hobbitsModel.js');
 
+//----------------------------------------------------------------------------//
+// 
+// See ./api/server.spec.js for some info on the jest methods etc. 
+// 
+//----------------------------------------------------------------------------//
+
 describe('hobbits model', () => {
-    describe('insert', () => {
-
-        beforeEach(async () => {
-            await db('hobbits').truncate();
-        });
-
-        it('should insert the provided hobbits into the db', async () => {
+    describe('insert()', () => {
+        it('should insert the provided hobbits into the DB', async () => {
             await Hobbits.insert({ name: 'gaffer' });
             await Hobbits.insert({ name: 'sam' });
 
             const hobbits = await db('hobbits');
             expect(hobbits).toHaveLength(2);
-            // expect(hobbits.length).toBe(2);
         });
 
-        it('should return the hobbit we inserted', async () => {
-            let hobbit = await Hobbits.insert({ name: 'sam' });
-            expect(hobbit.name).toBe('sam');
-
-            hobbit = await Hobbits.insert({ name: 'gaffer' });
+        it('should return what was inserted', async () => {
+            let hobbit = await Hobbits.insert({ name: 'gaffer' });
             expect(hobbit.name).toBe('gaffer');
+
+            hobbit = await Hobbits.insert({ name: 'sam' });
+            expect(hobbit.name).toBe('sam');
         });
 
-        it('is pointless', () => {
-            expect(true).toBe(true);
-        });
+        //----------------------------------------------------------------------------//
+        // 
+        // jest.beforeEach() specifies a method that is executed before each test. You
+        // would use this to do any setup or value initialization needed before *every*
+        // test.
+        // 
+        //----------------------------------------------------------------------------//
+
+        beforeEach(async () => {
+            await db('hobbits').truncate();
+        })
     });
 });
