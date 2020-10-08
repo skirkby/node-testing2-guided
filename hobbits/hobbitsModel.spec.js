f44d6833ac34120d650089ab9c40940f39d2d5ce
Sean Kirkby <skrkby@kirkby.org>
2020-10-07 23:27:41 -0600

    Failure can't cope with persistence.

--- hobbits/hobbitsModel.spec.js
+++ hobbits/hobbitsModel.spec.js
@@ -1,16 +1,15 @@
-const Hobbits = require('./hobbitsModel.js');
 const db = require('../data/dbConfig.js');
+const Hobbits = require('./hobbitsModel.js');
 
+//----------------------------------------------------------------------------//
+// 
+// See ./api/server.spec.js for some info on the jest methods etc. 
+// 
+//----------------------------------------------------------------------------//
 
 describe('hobbits model', () => {
-
-    describe('insert', () => {
-
-        beforeEach(async () => {
-            await db('hobbits').truncate();
-        })
-
-        it('should have the right length after insert', async () => {
+    describe('insert()', () => {
+        it('should insert the provided hobbits into the DB', async () => {
             await Hobbits.insert({ name: 'gaffer' });
             await Hobbits.insert({ name: 'sam' });
 
@@ -18,15 +17,24 @@ describe('hobbits model', () => {
             expect(hobbits).toHaveLength(2);
         });
 
-        it('should return the object that we inserted', async () => {
+        it('should return what was inserted', async () => {
             let hobbit = await Hobbits.insert({ name: 'gaffer' });
             expect(hobbit.name).toBe('gaffer');
 
             hobbit = await Hobbits.insert({ name: 'sam' });
             expect(hobbit.name).toBe('sam');
+        });
+
+        //----------------------------------------------------------------------------//
+        // 
+        // jest.beforeEach() specifies a method that is executed before each test. You
+        // would use this to do any setup or value initialization needed before *every*
+        // test.
+        // 
+        //----------------------------------------------------------------------------//
+
+        beforeEach(async () => {
+            await db('hobbits').truncate();
         })
-
-    })
-
-
-})
+    });
+});
